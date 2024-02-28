428af581dc1215e1700288b99ed35ec4627b331f
Alexei <>
2024-02-28 10:40:25 +0200

    2c

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
 
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  )
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
@@ -19,13 +32,13 @@ const App = (props) => {
     setNotes(notes.concat(noteObject))
     setNewNote('')
   }
+
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
   }
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important === true)
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
@@ -40,12 +53,11 @@ const App = (props) => {
         )}
       </ul>
       <form onSubmit={addNote}>
-
         <input value={newNote} onChange={handleNoteChange} />
-
         <button type="submit">save</button>
       </form>
     </div>
   )
 }
-export default App 
+
+export default App
