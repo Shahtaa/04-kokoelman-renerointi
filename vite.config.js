b0583da69a98831f025f0537c62b8b1e86dbd4e8
Alexei <>
2024-04-30 10:30:28 +0300

    pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/04-kokoelman-renerointi",
   plugins: [react()],
   server: {
     proxy: {
